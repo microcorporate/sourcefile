6fd2b71f6b170c1af5138001dc7b467cbffdd5ec
Microcorporate Technologies (Pvt) Ltd <108064893+microcorporate@users.noreply.github.com>
2024-01-18 19:08:54 +0500

    changes

--- enums_maps.dart
+++ enums_maps.dart
@@ -1,22 +1,4 @@
 
-
-class ViewChartOfAccountsPage extends StatefulWidget {
-  final Widget pageDrawer;
-  final Widget pageNavigation;
-  final PreferredSize pageAppBar;
-
-  const ViewChartOfAccountsPage({
-    Key? key,
-    required this.pageDrawer,
-    required this.pageNavigation,
-    required this.pageAppBar,
-  }) : super(key: key);
-
-  @override
-  State<ViewChartOfAccountsPage> createState() =>
-      _ViewChartOfAccountsPageState();
-}
-
 class _ViewChartOfAccountsPageState extends State<ViewChartOfAccountsPage> {
   @override
   Widget build(BuildContext context) {
