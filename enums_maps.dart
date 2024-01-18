67a297d0a8e3ab99d5d280afe8453c6659e2ec0e
Microcorporate Technologies (Pvt) Ltd <108064893+microcorporate@users.noreply.github.com>
2024-01-18 19:09:41 +0500

    change

--- enums_maps.dart
+++ enums_maps.dart
@@ -1,17 +1,1 @@
 
-class _ViewChartOfAccountsPageState extends State<ViewChartOfAccountsPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: widget.pageAppBar,
-      drawer: widget.pageDrawer,
-      bottomNavigationBar: widget.pageNavigation,
-      body: const Center(
-        child: Text(
-          'ViewChartOfAccountsPage',
-          style: TextStyle(fontSize: 24.0),
-        ),
-      ),
-    );
-  }
-}
